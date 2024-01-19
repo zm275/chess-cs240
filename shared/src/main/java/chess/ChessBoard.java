07e36444fa94f62742a00df677162b2796f03b2d
= <zm275@byu.edu>
2024-01-19 11:32:58 -0700

    chessboard tests all pass

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Map;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,9 +10,41 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "squares=" + Arrays.toString(squares) +
+                '}';
+    }
+
     private final ChessPiece[][]  squares = new ChessPiece[8][8];
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that = (ChessBoard) o;
+        return Arrays.deepEquals(squares, that.squares);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(squares);
+    }
+
     public ChessBoard() {
 
+    }
+    final static Map<Character, ChessPiece.PieceType> charToTypeMap = Map.of(
+            'p', ChessPiece.PieceType.PAWN,
+            'n', ChessPiece.PieceType.KNIGHT,
+            'r', ChessPiece.PieceType.ROOK,
+            'q', ChessPiece.PieceType.QUEEN,
+            'k', ChessPiece.PieceType.KING,
+            'b', ChessPiece.PieceType.BISHOP);
+    public ChessPiece[][] getSquares(){
+        return squares;
     }
 
     /**
@@ -32,6 +67,9 @@ public class ChessBoard {
     public ChessPiece getPiece(ChessPosition position) {
         return squares[position.getRow() - 1][position.getColumn() - 1];
     }
+    public void setPiece(ChessPosition position){
+        squares[position.getRow() - 1][position.getColumn() - 1] = null;
+    }
 
     public boolean isValidPosition(ChessPosition position){
         return position.getRow() <= 8 && position.getRow() >= 1 && position.getColumn() <= 8 && position.getColumn() >= 1;
@@ -46,6 +84,45 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        for (int i = 0; i <=7; i++){
+            for (int j = 0; j <= 7; j++){
+                squares[i][j] = null;
+            }
+        }
+        int row = 8;
+        int column = 1;
+        var boardText = """
+                |r|n|b|q|k|b|n|r|
+                |p|p|p|p|p|p|p|p|
+                | | | | | | | | |
+                | | | | | | | | |
+                | | | | | | | | |
+                | | | | | | | | |
+                |P|P|P|P|P|P|P|P|
+                |R|N|B|Q|K|B|N|R|
+                """;
+
+        for (var c : boardText.toCharArray()) {
+            switch (c) {
+                case '\n' -> {
+                    column = 1;
+                    row--;
+                }
+                case ' ' -> column++;
+                case '|' -> {
+                }
+                default -> {
+                    ChessGame.TeamColor color = Character.isLowerCase(c) ? ChessGame.TeamColor.BLACK
+                            : ChessGame.TeamColor.WHITE;
+                    var type = charToTypeMap.get(Character.toLowerCase(c));
+                    var position = new ChessPosition(row, column);
+                    var piece = new ChessPiece(color, type);
+                    this.addPiece(position, piece);
+                    column++;
+                }
+            }
+
+        }
+        int q = 4;
     }
 }
